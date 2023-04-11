23019efde84e39cc3fc6471f1f4ed07e08c3f4fa
Ana <ana.mrzv@mail.ru>
2023-04-12 00:12:35 +0300

    first test for login

--- src/test/java/project/LoginTest.java
+++ src/test/java/project/LoginTest.java
@@ -1,21 +1,42 @@
 package project;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import project.pages.LoginPage;
+import project.pages.MarketPage;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LoginTest {
 
+    public static WebDriver driver;
+    public static MarketPage marketPage;
+    public static LoginPage loginPage;
     @BeforeAll
     public static void setup() {
         //определение пути до драйвера и его настройка
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        marketPage = new MarketPage(driver);
+        loginPage = new LoginPage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(ConfProperties.getProperty("marketpage"));
+    }
+
+    @Test
+    public void loginTest(){
+        marketPage.clickLoginBtn();
+        loginPage.inputLogin(ConfProperties.getProperty("testlogin"));
+        loginPage.clickLoginBtn();
+        loginPage.inputPasswd("testpassword");
+        loginPage.clickLoginBtn();
+
+        assertEquals(ConfProperties.getProperty("testmail"), marketPage.checkUserIcon());
     }
 }
--- src/test/java/project/pages/MarketPage.java
+++ src/test/java/project/pages/MarketPage.java
@@ -12,11 +12,19 @@ public class MarketPage {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
-
     @FindBy(xpath = "//*[contains(text(), 'Войти')]/..")
     private WebElement loginBtn;
+    @FindBy(xpath = "//*[contains(@src, '//avatars.mds.yandex.net/get-yapic/0/0-0/islands-retina-middle')]/..")
+    private WebElement userIcon;
+    @FindBy(xpath = "//*[contains(@class, '_3ckxI _3L43M')]/..")
+    private WebElement userMail;
 
     public void clickLoginBtn() {
         loginBtn.click();
+    }
+
+    public String checkUserIcon() {
+        if (userIcon != null && userMail != null) return userMail.getText();
+        else return null;
     }
 }
